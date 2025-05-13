560368e6e2d6799ea4525ca9ff1d15e78d12afc5
mzrdmk <mzr.dmk@gmail.com>
2025-05-13 15:11:11 +0100

    Add another test for index creation

--- sql_bridge/tests/create_index.rs
+++ sql_bridge/tests/create_index.rs
@@ -1,7 +1,7 @@
 use sql_bridge::{MySqlDialect, PostgreSqlDialect, SQLiteDialect, parse};
 
 #[test]
-fn create_index() {
+fn create_unique_index() {
     let input = "CREATE UNIQUE INDEX idx ON table_name (id, org)";
     let mut ast = parse(input).unwrap();
     assert!(ast.len() == 1);
@@ -20,5 +20,29 @@ fn create_index() {
     assert_eq!(
         ast.to_sql(SQLiteDialect {}).unwrap(),
         "CREATE UNIQUE INDEX `idx` ON `table_name` (`id`, `org`)"
+    );
+}
+
+
+#[test]
+fn create_index() {
+    let input = "CREATE INDEX idx ON table_name (id, org)";
+    let mut ast = parse(input).unwrap();
+    assert!(ast.len() == 1);
+    let ast = ast.pop().unwrap();
+
+    assert_eq!(
+        ast.to_sql(MySqlDialect {}).unwrap(),
+        "CREATE INDEX `idx` ON `table_name` (`id`, `org`)"
+    );
+
+    assert_eq!(
+        ast.to_sql(PostgreSqlDialect {}).unwrap(),
+        "CREATE INDEX \"idx\" ON \"table_name\" (\"id\", \"org\")",
+    );
+
+    assert_eq!(
+        ast.to_sql(SQLiteDialect {}).unwrap(),
+        "CREATE INDEX `idx` ON `table_name` (`id`, `org`)"
     );
 }
