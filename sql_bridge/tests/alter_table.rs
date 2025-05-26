a3acc7fe4de6c11bb7bd3dc5b8b1856261313872
mzrdmk <mzr.dmk@gmail.com>
2025-05-26 20:08:57 +0100

    Implement alter table

---
+++ sql_bridge/tests/alter_table.rs
@@ -0,0 +1,64 @@
+use sql_bridge::{MySqlDialect, PostgreSqlDialect, SQLiteDialect, parse};
+
+#[test]
+fn rename_table() {
+    let query = "alter table test rename to foo";
+    let mut ast = parse(query).unwrap();
+    assert!(ast.len() == 1);
+    let ast = ast.pop().unwrap();
+
+    assert_eq!(
+        ast.to_sql(MySqlDialect {}).unwrap(),
+        "ALTER TABLE `test` RENAME TO `foo`",
+    );
+    assert_eq!(
+        ast.to_sql(PostgreSqlDialect {}).unwrap(),
+        "ALTER TABLE \"test\" RENAME TO \"foo\"",
+    );
+    assert_eq!(
+        ast.to_sql(SQLiteDialect {}).unwrap(),
+        "ALTER TABLE `test` RENAME TO `foo`",
+    );
+}
+
+#[test]
+fn add_column() {
+    let query = "alter table test add column foo int";
+    let mut ast = parse(query).unwrap();
+    assert!(ast.len() == 1);
+    let ast = ast.pop().unwrap();
+
+    assert_eq!(
+        ast.to_sql(MySqlDialect {}).unwrap(),
+        "ALTER TABLE `test` ADD COLUMN `foo` INT"
+    );
+    assert_eq!(
+        ast.to_sql(PostgreSqlDialect {}).unwrap(),
+        "ALTER TABLE \"test\" ADD COLUMN \"foo\" INT"
+    );
+    assert_eq!(
+        ast.to_sql(SQLiteDialect {}).unwrap(),
+        "ALTER TABLE `test` ADD COLUMN `foo` INTEGER"
+    );
+}
+
+#[test]
+fn drop_column() {
+    let query = "alter table test drop column foo";
+    let mut ast = parse(query).unwrap();
+    assert!(ast.len() == 1);
+    let ast = ast.pop().unwrap();
+
+    assert_eq!(
+        ast.to_sql(MySqlDialect {}).unwrap(),
+        "ALTER TABLE `test` DROP COLUMN `foo`"
+    );
+    assert_eq!(
+        ast.to_sql(PostgreSqlDialect {}).unwrap(),
+        "ALTER TABLE \"test\" DROP COLUMN \"foo\""
+    );
+    assert_eq!(
+        ast.to_sql(SQLiteDialect {}).unwrap(),
+        "ALTER TABLE `test` DROP COLUMN `foo`"
+    );
+}
