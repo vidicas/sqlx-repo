8e733cd08b22415fc6d75ba954aab78caf9daaa1
mkdmkz <65260773+mkdmkz@users.noreply.github.com>
2025-08-02 15:02:24 +0100

    Use sqlite connection options (#57)
    * Use sqlite connection options
    
    * Use connection params
    
    * Sqlite tests
    
    * Create_if_missing flag
    
    * Check error messages in tests

--- sqlx-repo-macros/src/expand.rs
+++ sqlx-repo-macros/src/expand.rs
@@ -354,37 +354,42 @@ impl Expander {
                     Result<Box<dyn #trait_name>, Box<dyn std::error::Error + Send + Sync + 'static>>
                 {
                     let mut database_url = url::Url::parse(database_url)?;
-                    let mut params: std::collections::HashMap<std::borrow::Cow<str>, std::borrow::Cow<str>> = database_url.query_pairs().collect();
+                    let mut params: std::collections::HashMap<String, String> = database_url
+                        .query_pairs()
+                        .map(|(key, value)| (key.to_string(), value.to_string()))
+                        .collect();
                     let db: Box<dyn #trait_name> = match database_url.scheme() {
                         "sqlite" => {
-                            if !params.contains_key("mode") {
-                                params.insert("mode".into(), "rwc".into());
-                            };
-                            let query = params
-                                .into_iter()
-                                .map(|(key, value)| format!("{key}={value}"))
-                                .collect::<Vec<_>>()
-                                .join("&");
-                            database_url.set_query(Some(&query));
+                            database_url.set_query(None);
+                            let mut sqlite_options: sqlx::sqlite::SqliteConnectOptions = database_url
+                                .as_str()
+                                .parse()?;
+                            sqlite_options = sqlite_options
+                                .foreign_keys(true)
+                                .create_if_missing(true);
+                            if let Some(param) = params.get("foreign_keys") && (param == "off" || param == "false") {
+                                sqlite_options = sqlite_options.foreign_keys(false);
+                            }
+                            if let Some(param) = params.get("create_if_missing") && (param == "off" || param == "false") {
+                                sqlite_options = sqlite_options.create_if_missing(false);
+                            }
+                            let pool = sqlx::Pool::<sqlx::Sqlite>::connect_with(sqlite_options).await?;
                             Box::new(
-                                DatabaseRepository::<sqlx::Sqlite>::new(
-                                    database_url.as_ref(),
-                                )
-                                .await?,
+                                DatabaseRepository::new( database_url.as_ref(), pool).await?
                             )
                         }
-                        "postgres" => Box::new(
-                            DatabaseRepository::<sqlx::Postgres>::new(
-                                database_url.as_ref(),
+                        "postgres" => {
+                            let pool = sqlx::Pool::<sqlx::Postgres>::connect(database_url.as_str()).await?;
+                            Box::new(
+                                DatabaseRepository::new( database_url.as_str(), pool ).await?
                             )
-                            .await?,
-                        ),
-                        "mysql" => Box::new(
-                            DatabaseRepository::<sqlx::MySql>::new(
-                                database_url.as_ref(),
+                        },
+                        "mysql" => {
+                            let pool = sqlx::Pool::<sqlx::MySql>::connect(database_url.as_str()).await?;
+                            Box::new(
+                                DatabaseRepository::new(database_url.as_str(), pool).await?
                             )
-                            .await?,
-                        ),
+                        },
                         unsupported => Err(format!("unsupported database: {unsupported}"))?,
                     };
                     Ok(db)
@@ -754,9 +759,7 @@ where
 
         assert!(
             pretty_expanded_item == expected,
-            "got:\n{}\nexpected:\n{}\n",
-            pretty_expanded_item,
-            expected
+            "got:\n{pretty_expanded_item}\nexpected:\n{expected}\n"
         );
 
         let expected = "\
@@ -844,9 +847,7 @@ pub trait Repo {
         let generated_trait = prettify(syn::parse2(trait_impl).unwrap());
         assert!(
             generated_trait == expected,
-            "got:\n{}\nexpected:\n{}\n",
-            generated_trait,
-            expected
+            "got:\n{generated_trait}\nexpected:\n{expected}\n"
         );
     }
 
---
+++ sqlx-repo/tests/test_sqlite.rs
@@ -0,0 +1,103 @@
+use std::ops::Deref;
+
+use anyhow::Result;
+use sqlx_repo::prelude::*;
+use tempfile::NamedTempFile;
+
+#[repo(Send + Sync + std::fmt::Debug)]
+impl Repo for DatabaseRepository {
+    async fn create_tables_and_insert_data(&self) -> Result<()> {
+        let query = query!(
+            "
+            create table foo(id int primary key);
+            create table bar(id int primary key, foreign key (id) REFERENCES foo(id) on delete cascade);
+            insert into foo values(1), (2), (3);
+            insert into bar values(1), (2), (3);
+        "
+        );
+        sqlx::query(query).execute(&self.pool).await?;
+        Ok(())
+    }
+
+    async fn delete_all_foo(&self) -> Result<()> {
+        let query = query!("delete from foo");
+        sqlx::query(query).execute(&self.pool).await?;
+        Ok(())
+    }
+
+    async fn select_all_bar(&self) -> Result<Vec<i64>> {
+        let query = query!("select * from bar");
+        Ok(sqlx::query(query)
+            .fetch_all(&self.pool)
+            .await?
+            .into_iter()
+            .map(|row| row.get(0))
+            .collect())
+    }
+}
+
+#[tokio::test]
+async fn test_database_creation_defaults() {
+    let url = "sqlite::memory:";
+    let repo = <dyn Repo>::new(url).await.unwrap();
+    repo.create_tables_and_insert_data().await.unwrap();
+    assert_eq!(vec![1, 2, 3], repo.select_all_bar().await.unwrap(),);
+    repo.delete_all_foo().await.unwrap();
+    assert_eq!(Vec::<i64>::new(), repo.select_all_bar().await.unwrap(),)
+}
+
+#[tokio::test]
+async fn test_database_creation_foreign_key_not_enforced() {
+    let url = "sqlite::memory:?foreign_keys=false";
+    let repo = <dyn Repo>::new(url).await.unwrap();
+    repo.create_tables_and_insert_data().await.unwrap();
+    assert_eq!(vec![1, 2, 3], repo.select_all_bar().await.unwrap(),);
+    repo.delete_all_foo().await.unwrap();
+    assert_eq!(vec![1, 2, 3], repo.select_all_bar().await.unwrap(),)
+}
+
+struct TmpFile(NamedTempFile);
+
+impl Deref for TmpFile {
+    type Target = NamedTempFile;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Drop for TmpFile {
+    fn drop(&mut self) {
+        std::fs::remove_file(self.path()).ok();
+    }
+}
+
+#[tokio::test]
+async fn test_database_creation_pre_existing_file() {
+    let file = TmpFile(tempfile::NamedTempFile::new().unwrap());
+    let url = format!("sqlite://{}", file.path().to_str().unwrap());
+    assert!(<dyn Repo>::new(&url).await.is_ok());
+}
+
+#[tokio::test]
+async fn test_database_creation_disallow_creation() {
+    let file = TmpFile(tempfile::NamedTempFile::new().unwrap());
+    let url = format!(
+        "sqlite://{}/?create_if_missing=false",
+        file.path().to_str().unwrap()
+    );
+    drop(file);
+    let res = <dyn Repo>::new(&url).await;
+    assert!(res.is_err());
+    assert_eq!(
+        "error returned from database: (code: 14) unable to open database file",
+        res.unwrap_err().to_string()
+    )
+}
+
+#[tokio::test]
+async fn test_database_default_creation() {
+    let file = TmpFile(tempfile::NamedTempFile::new().unwrap());
+    let url = format!("sqlite://{}", file.path().to_str().unwrap());
+    drop(file);
+    assert!(<dyn Repo>::new(&url).await.is_ok());
+}
